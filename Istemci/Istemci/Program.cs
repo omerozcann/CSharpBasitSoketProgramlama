48af8bfc9a99974de958ab7084138b71ee5aea56
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2022-03-02 21:44:03 +0300

    .

---
+++ Istemci/Istemci/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Istemci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpclnt = new TcpClient();            
+            tcpclnt.Connect("127.0.0.1", 8001);
+            Console.WriteLine("Bağlanıyor.....");
+
+            Console.WriteLine("Bağlandı");
+
+            Console.Write("Gönderilecek mesajı giriniz: ");
+            String str = Console.ReadLine();
+            Stream stm = tcpclnt.GetStream();
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] ba = asen.GetBytes(str);
+            Console.WriteLine("Gönderiliyor.....");
+
+            stm.Write(ba, 0, ba.Length);
+
+            byte[] bb = new byte[100];
+            int k = stm.Read(bb, 0, 100);
+
+            for (int i = 0; i < k; i++)
+                Console.Write(Convert.ToChar(bb[i]));
+
+            tcpclnt.Close();
+        }
+    }
+}
---
+++ Sunucu/Sunucu/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Sunucu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAd = IPAddress.Parse("127.0.0.1");        
+
+            TcpListener myList = new TcpListener(ipAd, 8001);
+            myList.Start();
+
+            Console.WriteLine("Sunucu 8001 numaralı portta çalışıyor...");
+            Console.WriteLine("Tam sunucu adresi  :" +myList.LocalEndpoint);
+            Console.WriteLine("Bağlantı için bekleniyor.....");
+
+            Socket s = myList.AcceptSocket();
+            Console.WriteLine("Bağlantı kabul edildi " + s.RemoteEndPoint);
+
+            byte[] b = new byte[100];
+            int k = s.Receive(b);
+            Console.WriteLine("Alındı...");
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write(Convert.ToChar(b[i]));
+            }
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            s.Send(asen.GetBytes("Mesaj sunucu tarafından alındı."));
+            Console.WriteLine("\nGönderim başarılı");
+            
+            s.Close();
+            myList.Stop();
+        }
+    }
+}
